83ce7d081f7dd2b5042cb57ed05ef694b0ddc23d
hasee-linux <larawsw@outlook.com>
2020-07-18 17:13:02 +0800

    fix: config init bash

--- local-config-init.sh
+++ local-config-init.sh
@@ -8,7 +8,7 @@ for file in ${config_list[@]}; do
   if [ -f $file ]; then
     echo "$file exists."
   else
-    cp $(echo 'db-local.php' | grep -oP '(.*)(?=-local)' | sed 's/$/.php /') $file
+    cp $(echo $file | grep -oP '(.*)(?=-local)' | sed 's/$/.php/') $file
     echo "$file is created success"
   fi
 done
